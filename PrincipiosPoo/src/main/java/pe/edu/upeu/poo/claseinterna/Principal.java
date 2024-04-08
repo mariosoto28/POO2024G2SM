b713ce83b13cd78a5cca44db523ff10c092a9269
luchi456 <torresaraseli520@gmail.com>
2024-04-08 10:47:40 -0500

    prueba

--- PrincipiosPoo/src/main/java/pe/edu/upeu/poo/claseinterna/Principal.java
+++ PrincipiosPoo/src/main/java/pe/edu/upeu/poo/claseinterna/Principal.java
@@ -14,6 +14,6 @@ public class Principal {
         System.out.println("La mama provee"+ma.alimento);
         Mama.Bebe bb=ma.new Bebe();
         System.out.println("Su bebe tiene un peso "+bb.peso+"kg");
-             System.out.println("");
+             System.out.println("cambios en mama hijo");
     }
 }
